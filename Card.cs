2b82610d74af84f80b33ca92a3821b9f88500846
kerokero014 <101151724+kerokero014@users.noreply.github.com>
2022-10-04 14:33:13 -0600

    Things off camera

---
+++ Card.cs
@@ -0,0 +1,14 @@
+
+namespace unit02_hilo
+{
+    class Card
+    {
+        public int cardValue;
+
+        ///Create a class for a card/draws a card
+        public void getNewCard(){
+            Random randomGenerator = new Random();
+            cardValue = randomGenerator.Next(1,14);
+        }
+    }
+}
---
+++ Project.cs
@@ -0,0 +1,12 @@
+
+namespace unit02_hilo
+{
+    class Program{
+        ///Begins game
+        static int Main(string[] args){
+            Director director = new Director();
+            director.StartGame();
+            return 0;
+        }
+    }
+}
