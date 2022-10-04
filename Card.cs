02eca99b6b214324b0089ea2a88ebd35afc07fd1
kerokero014 <101151724+kerokero014@users.noreply.github.com>
2022-10-04 14:38:34 -0600

    small change in card.cs

--- Card.cs
+++ Card.cs
@@ -8,7 +8,7 @@ namespace unit02_hilo
         ///Create a class for a card/draws a card
         public void getNewCard(){
             Random randomGenerator = new Random();
-            cardValue = randomGenerator.Next(1,14);
+            cardValue = randomGenerator.Next(1,13);
         }
     }
 }
